4c45be7d996c68370297dce55a7d6649e94bb08f
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2023-08-19 22:13:08 +0530

    set map in search_place

--- src/pages/Owner/Plots/plot_form/PlotForm.tsx
+++ src/pages/Owner/Plots/plot_form/PlotForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
@@ -6,14 +6,18 @@ import Nav from '../../../../share/nav/Nav'
 import style from './form.module.css'
 import Btn from '../../../../components/button/Btn';
 import { plotForm } from '../../../../service/ownerApi';
+import { useNavigate } from 'react-router-dom';
 
 // type Props = {}
 
 const PlotForm: React.FC = () => {
 
+  const navigate = useNavigate()
+
   // const [images, setImages] = useState<File[]>([]);
 
   const [placename , setPlotname]=useState('')
+  const [center , setCenter]=useState('')
   const [hour , setHour]=useState('')
   const [day , setDay]=useState('')
   const [month , setMonth]=useState('')
@@ -25,11 +29,11 @@ const PlotForm: React.FC = () => {
   const handleForm =  (e: any) => {
     e.preventDefault()
     const formData = {
-      placename,hour,day,month,location,plotdetails
+      center,placename,hour,day,month,location,plotdetails
     }
     console.log(formData);
-    
    plotForm(formData)
+   navigate('/owner')
   }
 
 
@@ -38,36 +42,44 @@ const PlotForm: React.FC = () => {
     <>
     <div className={style.form_image}>
       <Nav />
-      <div className='container' style={{ display: "flex", justifyContent: "flex-end" }}>
+      <div className='container' style={{ display: "flex", justifyContent: "center" }}>
         <Form >
+        <Row  className="mb-3">
+            <Form.Group >
+              <Form.Label>Park Center Name</Form.Label>
+              <Form.Control onChange={(e)=>setCenter(e.target.value)} type="text" placeholder="Name" required />
+            </Form.Group>
+          </Row>
           <Row  className="mb-3">
-            <Form.Group as={Col} sm="2" md="4">
+            <Form.Group>
               <Form.Label>Place Name</Form.Label>
               <Form.Control onChange={(e)=>setPlotname(e.target.value)} type="text" placeholder="Place Name" required />
             </Form.Group>
           </Row>
-          <Row className="mb-3">
+          <Row  style={{display:"flex",justifyContent:"center"}} className={`mb-3`}>
             {/* <div style={{ display: "flex", alignItems: "flex-end" }}> */}
+            <Form.Group>
             <Form.Label>Price</Form.Label>
-            <Form.Group as={Col} sm="2" md="2">
+            {/* <Form.Group as={Col} sm="2" md="2"> */}
               <Form.Control onChange={(e)=>setHour(e.target.value)} type="text" placeholder='Hour' required />
-            </Form.Group>
-            <Form.Group as={Col} sm="2" md="2">
+            {/* </Form.Group> */}
+            {/* <Form.Group as={Col} sm="2" md="2"> */}
               {/* <Form.Label>1Day</Form.Label> */}
               <Form.Control onChange={(e)=>setDay(e.target.value)} type="text" placeholder='Day' required />
-            </Form.Group>
-            <Form.Group as={Col} sm="3" md="2">
+            {/* </Form.Group> */}
+            {/* <Form.Group as={Col} sm="3" md="2"> */}
               <Form.Control onChange={(e)=>setMonth(e.target.value)} type="text" placeholder="Month" required />
+            {/* </Form.Group> */}
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} sm="2" md="2">
+            <Form.Group >
               <Form.Label>Location</Form.Label>
               <Form.Control onChange={(e)=>setlocation(e.target.value)} type="text" placeholder="Sub District" required />
             </Form.Group>
           </Row>
           <Row className="mb-3">
-            <Form.Group as={Col} md="4">
+            <Form.Group >
               <Form.Label>Plot Image</Form.Label>
               <Form.Control type="file" placeholder="image" required />
             </Form.Group>
@@ -86,7 +98,7 @@ const PlotForm: React.FC = () => {
             </Form.Group> */}
           </Row>
           <Row className="mb-3" style={{ display: "flex", alignItems: "flex-end" }}>
-            <Form.Group as={Col} md="4">
+            <Form.Group >
               <Form.Label>Plot Details</Form.Label>
               <Form.Control onChange={(e)=>setPlotdetails(e.target.value)} as="textarea" rows={3} placeholder="Details" required />
             </Form.Group>
--- src/pages/User/search_place/Place.tsx
+++ src/pages/User/search_place/Place.tsx
@@ -1,80 +1,111 @@
-import React, { useEffect, useState } from 'react'
-import Nav from '../../../share/nav/Nav'
-import style from './Place.module.css'
-import Search from '../../../components/search/Search'
+import React, { useEffect, useState } from 'react';
+import Nav from '../../../share/nav/Nav';
+import style from './Place.module.css';
+import Search from '../../../components/search/Search';
 import Image from 'react-bootstrap/Image';
-// import Card from '../../../share/card/Card'
-import { useNavigate } from 'react-router-dom'
-import { Plots } from '../../../service/userApi'
+import { useNavigate } from 'react-router-dom';
+import { Plots } from '../../../service/userApi';
 
 const Place: React.FC = () => {
   const [Data, setData] = useState([]);
-  const [state, setstate] = useState(false)
+  const [selectedCardId, setSelectedCardId] = useState(null);
   const navigate = useNavigate();
 
   const handleCard = () => {
     navigate('/plot');
-  }
+  };
 
-  const handleLocation = () =>{
-
-  }
+  const handleLocation = (id: any) => {
+    setSelectedCardId(id);
+  };
 
   useEffect(() => {
-    Plots()
-      .then((res) => {
-        if (res.data === "fail") {
-          alert("not fetched");
-        } else {
-          const data = res.data;
-          setData(data);
-        }
-      });
+    Plots().then((res) => {
+      if (res.data === 'fail') {
+        alert('not fetched');
+      } else {
+        const data = res.data;
+        setData(data);
+      }
+    });
   }, []);
 
   return (
     <div className='container-fluid'>
       <Nav />
-      <div className={style.search_time}>
-        <Search />
-        <div className={style.time_date}>
-          <div>
-            <input type="time" />
-            <input type="date" name="" id="" />
+      <div className="row">
+        <div className={`col-sm-12 col-md-12 col-lg-12 ${style.search_time}`}>
+          <Search />
+          <div className={style.time_date}>
+            <div>
+              <input defaultValue="02:30" type="time" />
+              <input defaultValue="02/08/2024" type="date" name="" id="" />
+            </div>
+            <div>
+              <input type="time" />
+              <input defaultValue="02/08/2024" type="date" name="" id="" />
+            </div>
           </div>
-          <div>
-            <input type="time" />
-            <input type="date" name="" id="" />
+        </div>
+
+        <div style={{ display: 'flex' }}>
+          <div className={style.alignCard}>
+            {Data.map((e: any, index: number) => (
+              <div
+                onClick={() => handleLocation(e._id)}
+                key={index}
+                className={`card col-12 ${style.responsiveCard}`}
+              >
+                <Image
+                  className={style.image}
+                  src=""
+                  rounded
+                />
+                <div className={` ${style.cardname}`}>
+                  <h6>Plot_Name: {e.center} </h6>
+                  <h6>City:{e.placename}</h6>
+                  <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    <label>Hour:{e.hour}</label>
+                    <label>Day:{e.day}</label>
+                  </div>
+                </div>               </div>
+            ))}
+          </div>
+          <div className={style.leftSide}>
+            {selectedCardId ? (
+              Data.map((e: any) => (
+                <div
+                  key={e._id}
+                  style={{ display: e._id === selectedCardId ? 'block' : 'none' ,height:"100%"}}
+                >
+                  {e._id === selectedCardId && (
+                    <iframe
+                      src={e.location} 
+                      width="100%"
+                      height="100%"
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  )}
+                </div>
+              ))
+            ) : (
+              <iframe
+                className='map'
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31303.856607630867!2d75.93269845414157!3d11.262727637715813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba64463e3f9e885%3A0xc470e3b737ca36ba!2sMavoor%2C%20Kerala%20673661!5e0!3m2!1sen!2sin!4v1692241553029!5m2!1sen!2sin"
+                width="100%"
+                height="100%"
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            )}
           </div>
         </div>
       </div>
-
-      <div className={style.alignCard}>
-        {Data.map((e: any, index: number) => (
-          <div onClick={handleLocation} key={index} className={`card ${style.responsiveCard}`}>
-                <Image
-                    className={style.image}
-                    src=""
-                    rounded
-                />
-                <div className={style.cardname}>
-                    <h6>Name: {e.placename} </h6>
-                    <h6>{}</h6>
-                    <h6></h6>
-                </div>
-                <iframe src={e.location} width="300" height="150" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-
-            </div>
-            
-        ))}
-      </div>
-      <div >
-
-      </div>
-
-      {/* <iframe src="" width="300" height="150"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
     </div>
-  )
-}
+  );
+};
 
 export default Place;
