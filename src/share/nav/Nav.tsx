10dca8a09abacc2afaa559c2918f133ca222d998
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2023-11-20 16:10:16 +0530

    commit

--- src/share/nav/Nav.tsx
+++ src/share/nav/Nav.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../../components/nav/Nav';
-import { isLoggedin } from '../../api/owner';
+import { isLoggedin, isLogout } from '../../api/owner'; // Import isLogout function
 import { useNavigate } from 'react-router-dom';
 import { userIsLoggedin } from '../../api/user';
-
 
 interface Props {
   name: string;
@@ -12,10 +11,33 @@ interface Props {
 
 const NavComponent: React.FC<Props> = ({ BtnAction, name }) => {
   const navigate = useNavigate();
+  const [isLoggedInState, setIsLoggedInState] = useState(false);
+
+  useEffect(() => {
+    // Check the login status when the component mounts
+    updateLoginStatus();
+
+    // Set up the beforeunload event listener
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    // Set up the unload event listener (for page refresh)
+    window.addEventListener('unload', handleUnload);
+
+    // Cleanup function for removing the event listeners when the component is unmounted
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.removeEventListener('unload', handleUnload);
+    };
+  }, []); // Empty dependency array ensures this effect runs once on mount
+
+  const updateLoginStatus = () => {
+    // Update the state based on the login status
+    setIsLoggedInState(name === 'owner' ? isLoggedin() : userIsLoggedin());
+  };
 
   const handleProfile = () => {
     navigate(`${BtnAction}/profile`);
-    const val = localStorage.getItem("owner");
+    const val = localStorage.getItem('owner');
     console.log(val);
   };
 
@@ -24,21 +46,23 @@ const NavComponent: React.FC<Props> = ({ BtnAction, name }) => {
     console.log(isLoggedin);
   };
 
+  const handleBeforeUnload = () => {
+    // Remove "owner" data from local storage when the user closes the site or refreshes
+    isLogout();
+  };
+
+  const handleUnload = () => {
+    // Handle the unload event if needed
+    // Note: This may not reliably run before the page refreshes due to browser behavior
+  };
+
   return (
     <>
-      {name === "owner" ? (
-        isLoggedin() ? (
-          <Nav BtnName="Profile" BtnAction={handleProfile} />
-        ) : (
-          <Nav BtnName="Login" BtnAction={handleLogin} />
-        )
-      ) : name === "user" ? (
-        userIsLoggedin() ? (
-          <Nav BtnName="Profile" BtnAction={handleProfile} />
-        ) : (
-          <Nav BtnName="Login" BtnAction={handleLogin} />
-        )
-      ) : null}
+      {isLoggedInState ? (
+        <Nav BtnName="Profile" BtnAction={handleProfile} />
+      ) : (
+        <Nav BtnName="Login" BtnAction={handleLogin} />
+      )}
     </>
   );
 };
