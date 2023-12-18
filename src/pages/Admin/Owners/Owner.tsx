f5b3f98cf6fadf97cd8171d2f8b1a5617b69da24
Mshanu-tech <muhammedshanu12345678910@gmail.com>
2023-12-18 17:41:10 +0530

    commit

--- src/pages/Admin/Owners/Owner.tsx
+++ src/pages/Admin/Owners/Owner.tsx
@@ -1,78 +1,73 @@
-import style from './owner.module.css'
-import Nav from '../../../share/nav/Nav'
-import Search from '../../../components/search/Search'
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { owners } from '../../../service/adminApi';
+import style from "./owner.module.css";
+import Nav from "../../../share/nav/Nav";
+import Search from "../../../components/search/Search";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { owners } from "../../../service/adminApi";
 // import Image from 'react-bootstrap/Image';
 // import { fetchImageURLs } from '../../../images/downloadIamge';
 
-type Props = {}
+type Props = {};
 
-function Owner({ }: Props) {
-    const [ownerData, setOwnerData] = useState([])
-    // const [OwnerimageURLs, setOwnerImageURLs] = useState<string[]>([]);
-    const [filteredOwners, setFilteredOwners] = useState([]);
-    const [query, setQuery] = useState('');
-    // console.log(OwnerimageURLs);
+function Owner({}: Props) {
+  const [ownerData, setOwnerData] = useState([]);
+  // const [OwnerimageURLs, setOwnerImageURLs] = useState<string[]>([]);
+  const [filteredOwners, setFilteredOwners] = useState([]);
+  const [query, setQuery] = useState("");
+  // console.log(OwnerimageURLs);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    owners().then((res) => {
+      if (res.data === "fail") {
+        alert("Not Data fetched");
+      } else {
+        const data = res.data.data.owner;
+        setOwnerData(data);
+        setFilteredOwners(data);
+      }
+    });
 
-    useEffect(() => {
+    // fetchImageURLs('owner/')
+    //     .then((urls) => {
+    //         setOwnerImageURLs(urls)
+    //     })
+    //     .catch((error) => {
+    //         console.error('Error fetching image URLs:', error);
+    //     })
+  }, []);
 
-        owners().then((res) => {
-            if (res.data === "fail") {
-                alert("Not Data fetched")
-            } else {
-                const data = res.data.data.owner
-                setOwnerData(data)
-                setFilteredOwners(data)
-            }
-        });
+  useEffect(() => {
+    const lowerCaseQuery = query.toLocaleLowerCase();
+    const filtered = ownerData.filter((owner) => {
+      const Data = owner.name.toLowerCase();
+      return Data.includes(lowerCaseQuery);
+    });
+    setFilteredOwners(filtered);
+  }, [query]);
 
-        // fetchImageURLs('owner/')
-        //     .then((urls) => {
-        //         setOwnerImageURLs(urls)
-        //     })
-        //     .catch((error) => {
-        //         console.error('Error fetching image URLs:', error);
-        //     })
-    }, [])
+  const handleOwner = (owner: any) => {
+    navigate(`/admin/owners/${owner._id}`);
+  };
+  const handleSearch = (inputValue: string) => {
+    setQuery(inputValue);
+  };
 
-    useEffect(() => {
-        const lowerCaseQuery = query.toLocaleLowerCase()
-        const filtered = ownerData.filter((owner) => {
-            const Data = owner.name.toLowerCase();
-            return Data.includes(lowerCaseQuery)
-        })
-        setFilteredOwners(filtered)
-    },[query])
+  return (
+    <>
+      <Nav BtnAction="asmin" name="user" />
 
-    const handleOwner = (owner:any) => {
-        navigate(`/admin/owners/${owner._id}`)
-    }
-    const handleSearch = (inputValue: string) =>{
-        setQuery(inputValue)
-    }
-
-    return (
-        <>
-            <Nav BtnAction="asmin" name='user' />
-
-            <div className='container-fluid'>
-                <div>
-                    <Search value={query} searchAction={handleSearch} />
-                </div>
-                <div className="row">
-                    <div className={style.align_card}>
-
-                        {
-                            filteredOwners.map((owner: any, index: number) => (
-                                <div  onClick={() => handleOwner(owner)} key={index} >
-
-                                    <div className={`card ${style.responsiveCard}`}>
-                                        {/* {OwnerimageURLs.map((image: string, imageIndex: number) => {
+      <div className="container-fluid">
+        <div>
+          <Search value={query} searchAction={handleSearch} />
+        </div>
+        <div className="row">
+          <div className={style.align_card}>
+            {filteredOwners.map((owner: any, index: number) => (
+              <div onClick={() => handleOwner(owner)} key={index}>
+                <div className={`card ${style.responsiveCard}`}>
+                  {/* {OwnerimageURLs.map((image: string, imageIndex: number) => {
 
                                             if (image.name === owner.image) {
                                                 return (
@@ -86,19 +81,16 @@ function Owner({ }: Props) {
                                             }
                                         })} */}
 
-                                        <div className={style.cardname}>
-                                            <h6>Name: {owner.name}</h6>
-                                            <h6>Phone: {owner.phone}</h6>
-                                            <h6>Email: {owner.email} </h6>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            ))
-                        }
-
-                    </div>
-                    {/* <div className={style.align_card}>
+                  <div className={style.cardname}>
+                    <h6>Name: {owner.name}</h6>
+                    <h6>Phone: {owner.phone}</h6>
+                    <h6>Email: {owner.email} </h6>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* <div className={style.align_card}>
                         <Card cardAction={handleowner} image='https://www.balloonsunlimitedchennai.com/cdn/shop/products/1_6959ed8f-fc51-4123-8fa2-be72fad48146_1800x.jpg?v=1659462374' />
                         <Card cardAction={handleowner} image='https://www.balloonsunlimitedchennai.com/cdn/shop/products/1_6959ed8f-fc51-4123-8fa2-be72fad48146_1800x.jpg?v=1659462374' />
                     </div>
@@ -107,10 +99,10 @@ function Owner({ }: Props) {
                         <Card cardAction={handleowner} image='https://www.balloonsunlimitedchennai.com/cdn/shop/products/1_6959ed8f-fc51-4123-8fa2-be72fad48146_1800x.jpg?v=1659462374' />
 
                     </div> */}
-                </div>
-            </div>
-        </>
-    )
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Owner
+export default Owner;
